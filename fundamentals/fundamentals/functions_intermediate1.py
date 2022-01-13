0d7b848c23870f2c0ca087ecce66349bbd4877a7
Lucky <lucky.maker1.618@gmail.com>
2022-01-13 13:26:44 -0600

    Completed Functions Intermediate I Assignment

--- fundamentals/fundamentals/functions_intermediate1.py
+++ fundamentals/fundamentals/functions_intermediate1.py
@@ -82,3 +82,38 @@
 # - 4 - #
 # Iterate Through a Dictionary with List Values
 # Create a function printInfo(some_dict) that, given a dictionary whose values are lists, prints the name of each key along with the size of its list, and then prints the associated values within each key's list.
+dojo = {
+   'locations': ['San Jose', 'Seattle', 'Dallas', 'Chicago', 'Tulsa', 'DC', 'Burbank'],
+   'instructors': ['Michael', 'Amy', 'Eduardo', 'Josh', 'Graham', 'Patrick', 'Minh', 'Devon']
+}
+
+def printInfo(dojo):
+    for key in dojo.keys():
+        print(len(dojo[key]), key.capitalize())
+        for i in dojo[key]:
+            print(i)
+
+        print("")
+
+printInfo(dojo)
+
+# Example Output:
+# 7 LOCATIONS
+# San Jose
+# Seattle
+# Dallas
+# Chicago
+# Tulsa
+# DC
+# Burbank
+    
+# 8 INSTRUCTORS
+# Michael
+# Amy
+# Eduardo
+# Josh
+# Graham
+# Patrick
+# Minh
+# Devon
+
