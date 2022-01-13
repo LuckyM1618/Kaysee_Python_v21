267720dab12a44e6d49eeae3f920701d42bf2696
Lucky <lucky.maker1.618@gmail.com>
2022-01-13 16:43:28 -0600

    Completed Bank Account Assignment

---
+++ fundamentals/oop/BankAccount.py
@@ -0,0 +1,53 @@
+class BankAccount:
+    # class atrribute
+    all_accounts = []
+
+    # don't forget to add some default values for these parameters!
+    # Default int_rate: 3, default balance: 0
+    def __init__(self, int_rate = 0.3, balance = 0):
+        # your code here! (remember, instance attributes go here)
+        self.balance = balance
+        self.int_rate = int_rate
+        BankAccount.all_accounts.append(self)
+        # don't worry about user info here; we'll involve the User class soon
+
+    @classmethod
+    def retrieve_accounts(cls):
+        for account in cls.all_accounts:
+            print(f"Interest Rate: {100 * account.int_rate}%, Balance: ${account.balance}")
+
+    def deposit(self, amount):
+        # your code here
+        self.balance += amount
+        return self
+
+    def withdraw(self, amount):
+        # your code here
+        if self.balance < amount:
+            print("Insufficient funds: Charging a $5 fee")
+            self.balance -= 5
+        else:
+            self.balance -= amount
+        
+        return self
+
+    def display_account_info(self):
+        # your code here
+        print(f"Balance: ${self.balance}")
+
+        return self
+
+    def yield_interest(self):
+        # your code here
+        if self.balance > 0:
+            self.balance *= (1 + self.int_rate)
+        
+        return self
+
+elric = BankAccount(0.08, 10000)
+rockbell = BankAccount(0.04, 5000)
+
+elric.deposit(2500).deposit(750).deposit(5000).withdraw(7500).yield_interest().display_account_info()
+rockbell.deposit(7500).deposit(2500).withdraw(800).withdraw(1300).yield_interest().display_account_info()
+
+BankAccount.retrieve_accounts()
