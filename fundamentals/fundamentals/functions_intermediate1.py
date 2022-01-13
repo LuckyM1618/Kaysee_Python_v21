c1bae2d8904d375eedee16fffe0516ea4b34bc6a
Lucky <lucky.maker1.618@gmail.com>
2022-01-12 18:09:19 -0600

    Working On Functions Intermediate I

---
+++ fundamentals/fundamentals/functions_intermediate1.py
@@ -0,0 +1,77 @@
+# # - 1. - # 
+# # Update Values in Dictionaries and Lists
+# x = [ [5,2,3], [10,8,9] ]
+
+# students = [
+#      {'first_name':  'Michael', 'last_name' : 'Jordan'},
+#      {'first_name' : 'John', 'last_name' : 'Rosales'}
+# ]
+
+# sports_directory = {
+#     'basketball' : ['Kobe', 'Jordan', 'James', 'Curry'],
+#     'soccer' : ['Messi', 'Ronaldo', 'Rooney']
+# }
+
+# z = [ {'x': 10, 'y': 20} ]
+
+# # 1.1 Change the value 10 in x to 15. Once you're done, x should now be [ [5,2,3],[15,8,9] ]
+# x[1][0] = 15
+# print(x)
+
+# # 1.2 Change the last_name of the first student from 'Jordan' to 'Bryant'
+# students[0]['last_name'] = "Bryant"
+# print(students)
+
+# # 1.3 In the sport_directory, change 'Messi' to 'Andres'
+# sports_directory['soccer'][1] = 'Andres'
+# print(sports_directory)
+
+# # 1.4 Change the value 20 in z to 30
+# z[0]['y'] = 30
+# print(z)
+
+# - 2. - #
+# Iterate Through a List of Dictionaries
+# Create a function iterateDictionary(some_list) that, given a list of dictionaries, the function loops through each dictionary in the list and prints each key and the associated value
+
+# ***note: students list is used for questions 2 and 3. Be careful not to comment out this code when testing only question 3
+students = [
+         {'first_name':  'Michael', 'last_name' : 'Jordan'},
+         {'first_name' : 'John', 'last_name' : 'Rosales'},
+         {'first_name' : 'Mark', 'last_name' : 'Guillen'},
+         {'first_name' : 'KB', 'last_name' : 'Tonel'}
+    ]
+
+def iterateDictionary(students):
+    for element in students:
+        output_str = ""
+        comma = True
+
+        for key,val in element.items():
+            output_str += key + " - " + val
+
+            if comma:
+                output_str += ", "
+                comma = False
+            else:
+                comma = True
+
+        print(output_str)
+
+iterateDictionary(students)
+
+# should output: (it's okay if each key-value pair ends up on 2 separate lines;
+# bonus to get them to appear exactly as below!)
+# Example Output:
+# first_name - Michael, last_name - Jordan
+# first_name - John, last_name - Rosales
+# first_name - Mark, last_name - Guillen
+# first_name - KB, last_name - Tonel
+
+# - 3. - #
+# Get Values From a List of Dictionaries
+# Create a function iterateDictionary2(key_name, some_list) that, given a list of dictionaries and a key name, the function prints the value stored in that key for each dictionary.
+
+def iterateDictionary2(key_name, some_list):
+    pass
+    # need to review navigating dictionaries to see if direct/effecient method or function exists 
