cac10b3dcdc909c1338f417f406e99fd4d1373cf
Lucky <lucky.maker1.618@gmail.com>
2022-01-14 17:28:17 -0600

    Completed Understanding Routing Assignment

--- flask/fundamentals/hello_flask/server.py
+++ flask/fundamentals/hello_flask/server.py
@@ -4,9 +4,24 @@ from flask import Flask
 app = Flask(__name__)    # Create a new instance of the Flask class called "app"
 
 @app.route('/')          # The "@" decorator associates this route with the function immediately following
-
 def hello_world():
     return 'Hello World!'  # Return the string 'Hello World!' as a response
+
+@app.route('/success')
+def success():
+    return "Great Success!!!"
+
+@app.route('/hello/<name>')
+def hello(name):
+    print(name)
+    return "Hello, " + name
+
+@app.route('/users/<username>/<user_id>')
+def show_user_profile(username, user_id):
+    print(username)
+    print(user_id)
+    return "Username: " + username + ", ID: " + user_id
+
 
 if __name__=="__main__":   # Ensure this file is being run directly and not from a different module    
     app.run(debug=True)    # Run the app in debug mode.
---
+++ flask/fundamentals/understanding_routing/server.py
@@ -0,0 +1,22 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def hello_world():
+    return "Hello World!"
+
+@app.route('/<user_input>')
+def dojo(user_input):
+    return user_input
+
+@app.route('/say/<str:name>')
+def say_hi(name):
+    return "Hi " + name + "!"
+
+@app.route('/repeat/<int:num>/<str:user_str>')
+def repeat(num, user_str):
+    return user_str * num
+
+if __name__=="__main__":
+    app.run(debug=True)
