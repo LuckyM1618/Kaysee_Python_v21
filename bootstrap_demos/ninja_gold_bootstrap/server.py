bd11e7b4d97b5cc7564c00a17a81a2c8e98c7bf6
Lucky <lucky.maker1.618@gmail.com>
2022-02-09 15:49:38 -0600

    Completed Ninja Gold with Bootstrap

--- bootstrap_demos/ninja_gold_bootstrap/server.py
+++ bootstrap_demos/ninja_gold_bootstrap/server.py
@@ -19,22 +19,32 @@ def process_money():
     if request.form['loc'] == 'farm':
         farm_payout = random.randint(10, 20)
         session['current_gold'] += farm_payout
-        messages.append(f'<p>You gained {farm_payout} gold from the farm!</p>')
+        messages.append(f'<p class=\"text-success\">You gained {farm_payout} gold from the farm!</p>')
 
     # Cave
     # earns 5 - 10 gold
-    if request.form['loc']  == 'cave':
-        session['current_gold'] = 20
+    elif request.form['loc']  == 'cave':
+        cave_payout = random.randint(5, 10)
+        session['current_gold'] += cave_payout
+        messages.append(f'<p class=\"text-success\">You gained {cave_payout} gold from the cave!</p>')
 
     # House
     # earns 2 - 5 gold
-    if request.form['loc']  == 'house':
-        session['current_gold'] = 30
+    elif request.form['loc']  == 'house':
+        house_payout = random.randint(2, 5)
+        session['current_gold'] += house_payout
+        messages.append(f'<p class=\"text-success\">You gained {house_payout} gold from the house!</p>')
 
     # Casino
     # earns/takes 0 - 50 gold
-    if request.form['loc']  == 'casino':
-        session['current_gold'] = 40
+    elif request.form['loc']  == 'casino':
+        casino_payout = random.randint(-50, 50)
+        if casino_payout >= 0:
+            messages.append(f'<p class=\"text-success\">You won {casino_payout} gold at the casino!</p>')
+        else:
+            messages.append(f'<p class=\"text-danger\">You lost {casino_payout} gold at the casino...Ouch!</p>')
+
+        session['current_gold'] += casino_payout
 
 
     return redirect('/')
